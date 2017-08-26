44df759d09481ae95ff6f5fed0d7e1ce5d76c813
ollin <oliver.nautsch@gmail.com>
2017-08-26 11:50:01 +0200

    current buy in + min raise

--- src/main/java/org/leanpoker/player/Card.java
+++ src/main/java/org/leanpoker/player/Card.java
@@ -11,6 +11,37 @@ public class Card {
 
     // TODO @ollin
     public int getRank() {
-        return 10;
+        switch (rank){
+            case "1":
+                return 1;
+            case "2":
+                return 2;
+            case "3":
+                return 3;
+            case "4":
+                return 4;
+            case "5":
+                return 5;
+            case "6":
+                return 6;
+            case "7":
+                return 7;
+            case "8":
+                return 8;
+            case "9":
+                return 9;
+            case "10":
+                return 10;
+            case "J":
+                return 11;
+            case "Q":
+                return 12;
+            case "K":
+                return 13;
+            case "A":
+                return 14;
+
+        }
+        return 1;
     }
 }
