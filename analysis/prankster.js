373cec4a291b1446d2ab6df8a8f272e1d1df832d
Hexagon SIX <hexagon6@fet.li>
2017-08-26 16:46:29 +0200

    wat?

--- analysis/prankster.js
+++ analysis/prankster.js
@@ -6,9 +6,11 @@ const data = require('./payload.json')
 const lodash = require('lodash')
 // Poker1up obscure-beyond-74265.herokuapp.com
 // funkyjokey
-const gameUrl = 'http://shielded-bastion-29985.herokuapp.com'
+const game_state = data
 
-axios.post(gameUrl, data)
-  .then(({ data }) => '.' + data)
+console.log(game_state.pot)
+const gameUrl = `http://shielded-bastion-29985.herokuapp.com/action=showdown&game_state=${game_state}`
+
+axios.post(gameUrl)
   .then(console.log)
   .catch(console.error)
