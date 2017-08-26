634fac049ded8861569113a4df6953a1fc1e91a1
syzer3@gmail.com <syzer3@gmail.com>
2017-08-26 16:40:09 +0200

    to you

--- analysis/prankster.js
+++ analysis/prankster.js
@@ -4,11 +4,11 @@ const axios = require('axios')
 const data = require('./payload.json')
 
 const lodash = require('lodash')
-// Poker1up obscure-beyond-74265.herokuapp.com 
+// Poker1up obscure-beyond-74265.herokuapp.com
 // funkyjokey
 const gameUrl = 'http://shielded-bastion-29985.herokuapp.com'
 
 axios.post(gameUrl, data)
-  .then(({ data }) => data)
+  .then(({ data }) => '.' + data)
   .then(console.log)
   .catch(console.error)
