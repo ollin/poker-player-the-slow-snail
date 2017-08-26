95a249427da5e169d47ebeb36f5b4540d2db6b95
syzer3@gmail.com <syzer3@gmail.com>
2017-08-26 17:02:41 +0200

    prankster

--- analysis/prankster.js
+++ analysis/prankster.js
@@ -8,11 +8,13 @@ const _ = require('lodash')
 const game_state = data
 
 // TODO fix '/action=showdown&game_state=[object%20Object]',
-const gameUrl = `http://shielded-bastion-29985.herokuapp.com/action=showdown&game_state=${game_state}`
+const gameUrl = `http://shielded-bastion-29985.herokuapp.com/action=showdown`
 
 Promise.all(
-  _.range(20)
-    .map(() => axios.post(gameUrl))
-).then((resps) => resps.map(({data})=> data))
-  .then(console.log)
-  .catch(console.error)
+  _.range(100)
+    .map(
+      () => axios.post(gameUrl, {
+        game_state: game_state
+      }).then(({ data }) => data)
+        .catch(({ data }) => data))
+).then(console.log)
