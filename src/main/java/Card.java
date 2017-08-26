79bc9a9fe919ca27233975babd5fcac4791efbb4
syzer3@gmail.com <syzer3@gmail.com>
2017-08-26 11:33:23 +0200

    card class

---
+++ src/main/java/Card.java
@@ -0,0 +1,9 @@
+/**
+ * Created by lukasgin on 26/08/2017.
+ */
+public class Card {
+
+    // TODO Integer
+    public String rank; // Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+    public String suit;  // Suit of the card. Possible values are: clubs,spades,hearts,diamonds
+}
