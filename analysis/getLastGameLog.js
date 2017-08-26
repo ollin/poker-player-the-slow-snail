43829d842d8ec88fc64494dbadd1072a0830892d
Hexagon SIX <hexagon6@fet.li>
2017-08-26 14:11:43 +0200

    fixed

--- analysis/getLastGameLog.js
+++ analysis/getLastGameLog.js
@@ -3,7 +3,7 @@
 const axios = require('axios')
 const lodash = require('lodash')
 
-const baseUrl = 'http://lumber.leanpoker.org/logs/tournament/'
+const baseUrl = 'http://lumber.leanpoker.org/'
 const gameUrl = 'http://lumber.leanpoker.org/logs/tournament/599b4b5f8042500004000002/team/poker-player-the-slow-snail/log'
 axios.get(gameUrl)
   .then(({ data }) => data)
