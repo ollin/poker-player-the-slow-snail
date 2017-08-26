5c14e60f2db6a7d71326aa8aec6b60f2fd1b4418
syzer3@gmail.com <syzer3@gmail.com>
2017-08-26 13:22:04 +0200

    parsing payers

--- analysis/count_fold_threshold.js
+++ analysis/count_fold_threshold.js
@@ -1,23 +1,12 @@
+// nodemon -w ./ -x 'cat count_fold_threshold.js | node -p'
 const response = require('./logdump.json')
 const threshold = require('./threshold')
 
-console.log(threshold)
-/*
-response.map(({ game_state: { players } }) => {
-	return players
-})
-.map((players) => {
-	return players.map(({ name, id, hole_cards }) => {
-		return {
-			name,
-			id,
-			hole_cards
-		}
-	})
-})
-*/
-/*
-.map(({ name, id }) => {
-	console.log(`${name} has ${id}`)
-})
-*/
+// Poker1Up
+// funkyjokey (id:0)
+response
+  .map(({ type, game_state: { players } }) =>
+    ({
+      type,
+      players: players.filter(e => e.id === 0)
+    }))
