cc7f8210576815c977c28a607d307655559941fa
syzer3@gmail.com <syzer3@gmail.com>
2017-08-26 16:52:52 +0200

    to you

--- analysis/prankster.js
+++ analysis/prankster.js
@@ -2,15 +2,17 @@
 
 const axios = require('axios')
 const data = require('./payload.json')
-
-const lodash = require('lodash')
+const _ = require('lodash')
 // Poker1up obscure-beyond-74265.herokuapp.com
 // funkyjokey
 const game_state = data
 
-console.log(game_state.pot)
+// TODO fix '/action=showdown&game_state=[object%20Object]',
 const gameUrl = `http://shielded-bastion-29985.herokuapp.com/action=showdown&game_state=${game_state}`
 
-axios.post(gameUrl)
+Promise.all(
+  _.range(20)
+    .map(() => axios.post(gameUrl))
+).then((resps) => resps.map(({data})=> data))
   .then(console.log)
   .catch(console.error)
